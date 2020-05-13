c0003496553361b510daa53c5910d6032ded1b6e
Paweł Kocot <pawel.kocot@fortedigital.no>
2020-05-13 23:41:32 +0200

    export EmailService

--- notifications-api/notifications-api.module.ts
+++ notifications-api/notifications-api.module.ts
@@ -3,6 +3,7 @@ import { NotificationsApiConfigDataInterface, NotificationsApiCoreModuleOptionsI
 import { NOTIFICATIONS_API_CONFIG_SERVICE, NotificationsApiCoreModule } from './notifications-api-core.module'
 import { NotificationApiConfig } from './config/notification-api.config'
 import { UnfoldUtilsModule } from '../unfold-utils/unfold-utils.module'
+import { EmailService } from './service/email.service'
 
 export class NotificationsApiModule {
   static async forAsyncRoot(options: NotificationsApiCoreModuleOptionsInterface): Promise<DynamicModule> {
@@ -15,8 +16,9 @@ export class NotificationsApiModule {
           useFactory: (config: NotificationsApiConfigDataInterface) => new NotificationApiConfig(config),
           inject: [NOTIFICATIONS_API_CONFIG_SERVICE],
         },
+        EmailService,
       ],
-      exports: [],
+      exports: [EmailService],
     }
   }
 }
