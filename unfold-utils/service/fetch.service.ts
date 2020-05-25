7831c2be635caa8497003f0a63839dbaeeca133f
Paweł Kocot <pawel.kocot@fortedigital.no>
2020-05-25 10:39:55 +0200

    export fetch options

--- unfold-utils/service/fetch.service.ts
+++ unfold-utils/service/fetch.service.ts
@@ -3,7 +3,7 @@ import * as fetch from 'node-fetch'
 
 export type FetchMethod = 'GET' | 'POST' | 'PUT'
 
-interface RequestOptionsInterface {
+export interface FetchOptionsInterface {
   readonly method: FetchMethod
   readonly url: string
   readonly data?: object | string
@@ -13,7 +13,7 @@ interface RequestOptionsInterface {
 
 @Injectable()
 export class FetchService {
-  call(options: RequestOptionsInterface): Promise<fetch.Response> {
+  call(options: FetchOptionsInterface): Promise<fetch.Response> {
     return fetch.default(options.url, {
       body: options.rawData ? options.rawData : options.data ? JSON.stringify(options.data) : undefined,
       headers: options.headers,
