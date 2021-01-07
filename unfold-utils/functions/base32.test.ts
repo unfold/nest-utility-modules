e041a7c3966412d57dab0173989f85acb156b4be
Paweł Kocot <pawel.kocot@fortedigital.no>
2021-01-07 16:47:52 +0100

    more tests

--- unfold-utils/functions/base32.test.ts
+++ unfold-utils/functions/base32.test.ts
@@ -3,9 +3,11 @@ import { decodeBase32, encodeBase32 } from './base32'
 describe('base32 test', () => {
   it('should decode string', async () => {
     expect(decodeBase32('edqput90edu74ubecwg78vt0chjp6vv4cm')).toBe('some string to decode')
+    expect(decodeBase32('5n6n2mub851mcg9d85rnmvthc8v4uhjt')).toBe('-MQSKACFA-AqZo1b6MFY')
   })
 
   it('should encode string', async () => {
     expect(encodeBase32('some string to encode')).toBe('edqput90edu74ubecwg78vt0cnq66vv4cm')
+    expect(encodeBase32('-MQSKACFA-AqZo1b6MFY')).toBe('5n6n2mub851mcg9d85rnmvthc8v4uhjt')
   })
 })
