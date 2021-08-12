204845feb60dae6f72c0bfd81df63e856f37e277
Paweł Kocot <pawel.kocot@fortedigital.no>
2021-08-13 01:44:15 +0200

    feat: withRetry new options

--- unfold-utils/functions/with-retry.ts
+++ unfold-utils/functions/with-retry.ts
@@ -1,33 +1,40 @@
 import { LoggerService } from '@nestjs/common'
 import { delay } from './delay'
 
+type DelayTimeProvider = (retryNumber: number) => Promise<number>
+
 interface WithRetryOptions {
   maxRetries?: number
   logger?: LoggerService
+  info?: string
+  delayTimeProvider?: DelayTimeProvider
 }
 
 // @ts-ignore
-export const withRetry = async <T>(fn: () => Promise<T>, options: WithRetryOptions): Promise<T> => {
-  const maxRetries = options.maxRetries || 3
-  const logger = options.logger || {
+export const withRetry = async <T>(fn: () => Promise<T>, options?: WithRetryOptions): Promise<T> => {
+  let retryNumber = 0
+
+  const maxRetries = options?.maxRetries || 3
+  const info = options?.info ? `[${options.info}]` : ''
+  const logger = options?.logger || {
     log: () => {
       /* nothing */
     },
   }
 
-  let retryNumber = 1
   while (retryNumber <= maxRetries) {
+    retryNumber++
+
     try {
       const result = await fn()
       return result
     } catch (error) {
-      const delayTime = 300 * retryNumber
-      logger.log(`[RetryService.withRetry] Attempt ${retryNumber}/${maxRetries} failed - ${error} - (waiting ${delayTime}ms before next retry)`)
+      const nextDelayTime = options?.delayTimeProvider ? await options.delayTimeProvider(retryNumber) : 300 * retryNumber
+      logger.log(`[withRetry] ${info} Attempt ${retryNumber}/${maxRetries} failed - ${error} - (waiting ${nextDelayTime}ms before next retry)`)
       if (retryNumber === maxRetries) {
         throw error
       }
-      await delay(delayTime)
-      retryNumber++
+      await delay(nextDelayTime)
     }
   }
 }
